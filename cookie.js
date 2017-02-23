a9c87e30ebb82a486fd9b2f8539b28325f7f9191
Jen Kagan <kaganjd@gmail.com>
2017-02-22 23:56:33 -0500

    update cookie.js

--- cookie.js
+++ cookie.js
@@ -4,11 +4,25 @@ var cookieParser = require('cookie-parser');
 var app = express();
 app.use(cookieParser());
 
-app.get('/', function(req, resp) {
-  resp.cookie('t_swift_cookie', 'she wears short skirts, i wear tshirts');
-  resp.end('has been here the whole time!');
+app.get('/', function(request, response) {
+  // Sets cookie key=value pair
+  response.cookie('userID', '8QWEr6Ty9');
+  // Ends get GET request and logs a message
+  response.end('Success!\n' + 'Check your cookies');
 });
 
 app.listen(4000, function() {
-  console.log('what youre looking for?')
+  console.log('\n\nApp is up and running\n' + 'Listening on Port 4000...')
 })
+
+// Bonus Taylor Swift cookie
+// Un-comment and replace lines 7-14 above with lines 19-26 below
+//
+// app.get('/', function(req, resp) {
+//   resp.cookie('tSwiftCookie', 'She_wears_short_skirts_I_wear_t-shirts');
+//   resp.end('...has been here the whole time!');
+// });
+
+// app.listen(4000, function() {
+//   console.log('\n\nWhat youre looking for...')
+// })
